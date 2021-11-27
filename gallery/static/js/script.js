d3b4babea56f731df1f82402bdcfe5180ec61dd3
mary-wan <mary.njenga@student.moringaschool.com>
2021-11-27 17:00:53 +0300

    add image category and locations

--- gallery/static/js/script.js
+++ gallery/static/js/script.js
@@ -1,7 +1,15 @@
 function copypath(id) {
     var path = document.getElementById(id);
     path.select();
-    path.setSelectionRange(0, 99999); /* For mobile devices */
+    path.setSelectionRange(0, 99999);
     navigator.clipboard.writeText(path.value);
     alert("Copied path: " + path.value);
   }
+//   $(document).ready(function () {
+//     $("imgdisplay").mouseover(function () {
+//         $("#photoDesc").show();
+//     });
+//     $(".imgdisplay").mouseout(function () {
+//         $("#photoDesc").hide();
+//     });
+//   });
