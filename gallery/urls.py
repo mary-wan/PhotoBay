d5bbb944579261951c93a8cddc06a9190b17cf96
mary-wan <mary.njenga@student.moringaschool.com>
2021-11-27 21:33:10 +0300

    add filter by location functionality

--- gallery/urls.py
+++ gallery/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
+from django.conf.urls import url
 from . import views
 
 urlpatterns=[
     path('',views.index,name='galleryHome'),
-    path('search/',views.get_category,name='category')
+    path('search/',views.get_category,name='category'),
+    path('location/<str:search_location>/',views.get_location,name='location'),
+    #  url(r'^location/(?P<image_location>\d+)', views.location_filter, name='location_filter'),
     
 ]
--- gallery/views.py
+++ gallery/views.py
@@ -7,19 +7,26 @@ def index(request):
     
     return render(request,'all-gallery/home.html',{'images': images, 'locations': locations})
 
-def get_category(request):
-    # categories= Category.objects.all()
-    # locations = Location.objects.all()
+def get_category(request):   
     title = "Category"
+    locations = Location.objects.all()
     
     if 'category' in request.GET and request.GET["category"]:
         search_category=request.GET.get("category")
         searched_category = Image.search_image(search_category)
         message = f"{search_category}"
         
-        return render(request,'all-gallery/search.html',{'message':message,'images':searched_category,'title':title})
+        return render(request,'all-gallery/search.html',{'message':message,'images':searched_category,'title':title,'locations':locations})
     else:
         message = "You haven't searched for any category"
         return render(request, 'all-gallery/search.html',{"message":message})
+    
+def get_location(request,search_location):
+    title = "Location"
+    locations = Location.objects.all()
+    images = Image.filter_by_location(search_location)
+    return render(request, 'all-gallery/location.html',{"images":images,'locations':locations,'title':title})
+
+    
         
     
